0166955819ff3917d72834ba189ad358223be994
andrew-up2242777 <up2242777@myport.ac.uk>
2025-02-20 20:21:10 +0000

    Add files via upload
    Statistics.jsx

---
+++ frontend/src/pages/Statistics.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Statistics() {
+  return (
+    <div>
+      <h1>Statistics Page</h1>
+      <h2> hello </h2>
+      <h3> if you see this, it's working! </h3>
+    </div>
+  );
+}
+
+export default Statistics;
